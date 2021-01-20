070be8796090faa6020cfd92f1c5124323aff06d
André Durão <andreluisdurao@gmail.com>
2021-01-20 20:41:33 -0300

    Add beep sound

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ function App() {
   // frequency: rate in drops per minute
   const [frequency, setFrequency] = useState(30);
   const [dripping, setDripping] = useState(true);
+  const [intervalID, setIntervalID] = useState();
   const invertState = () => {
     setDripping(!dripping);
+    if (dripping) {
+      clearInterval(intervalID);
+      setIntervalID(undefined);
+    }
   }
   const drop = () => {
     return dripping && <div className="drop" style={{"animation-duration": `${60.0 / frequency}s`}}> </div>;
@@ -20,8 +25,16 @@ function App() {
       setFrequency(frequency-1);
     }
   }
+  const beep = () => {
+    document.getElementById('play').play();
+  };
+  const startBeep = () => {
+    const duration = (60.0 / frequency) * 1000.0;
+    !!!intervalID && setIntervalID(setInterval(beep, duration));
+  }
   return (
     <div className="App">
+      <audio id="play" src={process.env.PUBLIC_URL + '/beep.mp3'}></audio>
       <header className="App-header">
         <div className="drip-container">
           { drop() }
@@ -33,6 +46,9 @@ function App() {
         </div>
         <div className="control-panel">
           <button type="button" className="start-button" onClick={invertState}>Start/Stop</button>
+        </div>
+        <div className="control-panel">
+          <button type="button" className="start-button" onClick={startBeep}>beep</button>
         </div>
       </header>
     </div>
