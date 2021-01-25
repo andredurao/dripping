43143dbb35fcce4cfc28fc21fc7833e71a09de41
André Durão <andreluisdurao@gmail.com>
2021-01-25 19:01:13 -0300

    - Use local storage to set the frequency - Remove beep button - Start stopped by default

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function updateLocalStorage(frequency) {
+  localStorage.setItem('drippingFrequency', frequency);
+}
+
+function fetchLocalStorage() {
+  let frequency = localStorage.getItem('drippingFrequency');
+  if (!!!frequency) {
+    return 60; // default value
+  } else {
+    return parseInt(frequency);
+  }
+}
+
 function App() {
   // frequency: rate in drops per minute
-  const [frequency, setFrequency] = useState(30);
-  const [dripping, setDripping] = useState(true);
+  const [frequency, setFrequency] = useState(fetchLocalStorage());
+  const [dripping, setDripping] = useState(false);
   const [intervalID, setIntervalID] = useState();
   const audio = new Audio('./beep.mp3');
   const invertState = () => {
@@ -13,16 +26,22 @@ function App() {
     if (dripping) {
       clearInterval(intervalID);
       setIntervalID(undefined);
+    } else {
+      audio.play().then(() => audio.pause());
+      const duration = (60.0 / frequency) * 1000.0;
+      !!!intervalID && setIntervalID(setInterval(beep, duration));
     }
   }
   const drop = () => {
     return dripping && <div className='drop' style={{'animation-duration': `${60.0 / frequency}s`}}> </div>;
   }
   const increaseFrequency = () => {
+    updateLocalStorage(frequency+1);
     setFrequency(frequency+1);
   }
   const decreaseFrequency = () => {
     if (frequency > 0){
+      updateLocalStorage(frequency-1);
       setFrequency(frequency-1);
     }
   }
@@ -34,6 +53,9 @@ function App() {
     const duration = (60.0 / frequency) * 1000.0;
     !!!intervalID && setIntervalID(setInterval(beep, duration));
   }
+  const buttonLabel = () => {
+    return dripping ? 'Stop ⏹' : 'Start ▶️';
+  }
   return (
     <div className='App'>
       <header className='App-header'>
@@ -44,14 +66,11 @@ function App() {
           <p className='text'>
             {frequency} drops per minute
           </p>
-          <button type='button' className='frequency' onClick={decreaseFrequency}>-</button>
-          <button type='button' className='frequency' onClick={increaseFrequency}>+</button>
+          <button type='button' className='frequency' disabled={dripping} onClick={decreaseFrequency}>-</button>
+          <button type='button' className='frequency' disabled={dripping} onClick={increaseFrequency}>+</button>
         </div>
         <div className='control-panel'>
-          <button type='button' className='start-button' onClick={invertState}>Start/Stop</button>
-        </div>
-        <div className='control-panel'>
-          <button type='button' className='start-button' onClick={startBeep}>beep</button>
+          <button type='button' className='start-button' onClick={invertState}>{buttonLabel()}</button>
         </div>
       </header>
     </div>
